67d47c445453e1353d5527c0abfc4e065f2510ce
arikenl2 <ariken235@gmail.com>
2026-05-26 12:03:29 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bonjour Ariken");
+
+    }
+
+}
+public class Etudiant{
+    String nom;
+    int age;
+    public Etudiant(String nom, int age) {
+        this.nom=nom;
+        this.age=age;
+        
+    }
+}public class Enseignant {
+    String nom;
+    String specialite;
+    public Enseignant(String nom, String sprcialite) {
+        this.nom=nom;
+        this.specialite=specialite;
+
+    }
+
+    
+}
+public class Enseignant {
+    String nom;
+    String specialite;
+    public Enseignant(String nom, String sprcialite) {
+        this.nom=nom;
+        this.specialite=specialite;
+
+    }
+
+    
+}
+public class Cours {
+    String nomCours;
+    int duree;
+    public Cours(String nomCours, int duree) {
+        this.nomCours=nomCours;
+        this.duree=duree;
+        
+    }
+}
